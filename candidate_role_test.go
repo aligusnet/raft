a53db2b6661818b1336feab227a617979a821f2b
Alexander Ignatyev <ignatyev.alexander@gmail.com>
2017-05-24 23:02:14 +0100

    candidateRole unit tests: requestVote processing

--- candidate_role_test.go
+++ candidate_role_test.go
@@ -1,6 +1,7 @@
 package raft
 
 import (
+	pb "github.com/alexander-ignatyev/raft/raft"
 	"context"
 	. "github.com/smartystreets/goconvey/convey"
 	"testing"
@@ -8,8 +9,8 @@ import (
 )
 
 func TestCandidateRole(t *testing.T) {
-	state := newState(1, time.Millisecond*10)
 	Convey("Replica should be elected given at least half of votes", t, func() {
+		state := newState(1, time.Millisecond*10)
 		var replicas []*Replica
 		for i := 0; i < 4; i++ {
 			client := newRequestVoteClient(i%2 == 0)
@@ -21,7 +22,40 @@ func TestCandidateRole(t *testing.T) {
 		So(rh, ShouldEqual, LeaderRoleHandle)
 	})
 
+	Convey("Replica should response on requestVote", t, func(c C) {
+		state := newState(1, time.Millisecond*10)
+		channels := newChannelSet()
+		var replicas []*Replica
+		for i := 0; i < 4; i++ {
+			client := newRequestVoteClient(i%2 == 0)
+			replicas = append(replicas, &Replica{client: client})
+		}
+
+		go func() {
+			time.Sleep(2*time.Millisecond)
+			peerState := newState(2, time.Millisecond*10)
+			peerState.currentTerm=state.currentTerm+1
+			peerState.log.Append(1, []byte("cmd1"))
+			request := peerState.requestVoteRequest()
+			resultCh := make(chan struct {
+				*pb.RequestVoteResponse
+				error
+			}, 1)
+			msg := &requestVoteMessage{context.Background(), request, resultCh}
+			channels.requestVoteCh <- msg
+			result := <-resultCh
+			c.So(result.VoteGranted, ShouldBeTrue)
+			c.So(result.error, ShouldBeNil)
+		}()
+
+		role := &CandidateRole{replicas: replicas, channels: channels}
+		rh, _ := role.RunRole(context.Background(), state)
+		c.So(rh, ShouldEqual, CandidateRoleHandle)
+	})
+
+
 	Convey("Replica should not be elected given less than half of votes", t, func() {
+		state := newState(1, time.Millisecond*10)
 		var replicas []*Replica
 		for i := 0; i < 4; i++ {
 			client := newRequestVoteClient(i == 0)
@@ -34,7 +68,7 @@ func TestCandidateRole(t *testing.T) {
 	})
 
 	Convey("Replica should not be elected given deadline passes", t, func() {
-		state.timeout = time.Millisecond
+		state := newState(1, time.Millisecond)
 		var replicas []*Replica
 		for i := 0; i < 4; i++ {
 			client := newRequestVoteClient(true)
@@ -47,7 +81,7 @@ func TestCandidateRole(t *testing.T) {
 	})
 
 	Convey("Replica should not be elected given cancelation", t, func() {
-		state.timeout = 10 * time.Millisecond
+		state := newState(1, time.Millisecond*10)
 		var replicas []*Replica
 		for i := 0; i < 4; i++ {
 			client := newRequestVoteClient(true)
